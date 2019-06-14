0437c912a4553a0f4e13dc3c4ea825fe0c0b9b8b
tushar <tuskibansal@gmail.com>
2019-06-14 12:03:27 +0530

    project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {AboutComponent} from './about/about.component';
+import {RoomsComponent} from './rooms/rooms.component';
+import {RestaurantComponent} from './restaurant/restaurant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blog', component:BlogComponent },
+  {path: 'rooms', component:RoomsComponent },
+  {path: 'restaurant', component:RestaurantComponent },
+  {path: 'about', component:AboutComponent },
+  {path: 'contact', component:ContactComponent },
+  {path: 'home', component:HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { GridComponent } from './navigation/grid/grid.component';
+import { TourComponent } from './navigation/tour/tour.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RoomsComponent,
+    RestaurantComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent,
+    GridComponent,
+    TourComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
