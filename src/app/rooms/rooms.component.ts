41f02b114b3f1dfbcae7b6186c1c1666699c4f56
tushar <tuskibansal@gmail.com>
2019-07-05 17:05:13 +0530

    comment

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -4,7 +4,7 @@ import { Http } from '@angular/http';
 // import 'rxjs/add/operator/map';
 
 import { map } from "rxjs/operators";
-
+//here addig ssomething important
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
